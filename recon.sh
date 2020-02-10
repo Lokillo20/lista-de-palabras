d14060487f7d6fb50fac899ce797eac742460024
Lokillo20 <48301463+Lokillo20@users.noreply.github.com>
2020-02-10 00:16:24 -0300

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in $(cat $1)
+do
+        mkdir ~/recon/$i
+        cd ~/recon/$i
+        source ~/.bashrc
+        certspotter $i | tee -a recon.certspotter.txt
+        certsh $i | tee -a recon.crtsh.txt
+        subfinder -d $i -o ~/recon/$i/recon.subfinder.txt
+        findomain -t $i -u ~/recon/$i/recon.findomain.txt
+        cat recon* | sort -u > all.recon.txt
+        cat all.recon.txt | httprobe -c 50 | tee -a httprobe.recon.txt
+        grep https httprobe.recon.txt | tee -a https443.recon.txt
+        grep -v https httprobe.recon.txt | tee -a http80.recon.txt
+done
