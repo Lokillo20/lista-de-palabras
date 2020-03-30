0c9837351bba5e01de4b13600d909324fa2566bb
Lokillo20 <48301463+Lokillo20@users.noreply.github.com>
2020-03-30 14:33:44 -0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -4,10 +4,12 @@ do
         mkdir ~/recon/$i
         cd ~/recon/$i
         crtsh $i | tee -a recon.crtsh.txt
-        subfinder -d $i -o ~/recon/$i/recon.subfinder.txt
+        assetfinder --sub-only $i > recon.asset.txt
         findomain -t $i -u ~/recon/$i/recon.findomain.txt
+        python3 ~/tools/github-search/github-subdomains.py -t c5070e393359bf9bfeaf870fc6ad8136070697af -d $i > recon.github.txt
         cat recon* | sort -u > all.recon.txt
         cat all.recon.txt | httprobe -c 50 | tee -a httprobe.recon.txt
         grep https httprobe.recon.txt | tee -a https443.recon.txt
         grep -v https httprobe.recon.txt | tee -a http80.recon.txt
 done
+
